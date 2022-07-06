15842534ffc00f78ece3139137562baac42917b5
AntsoTech <antso-rakotoson@hotmail.fr>
2022-07-06 23:52:46 +0200

    updated index to deploy cors - added true credentials

--- index.ts
+++ index.ts
@@ -11,25 +11,37 @@ const port = process.env.PORT || 3000;
 // CORS : J'autorise localhost:3001 et localhost:3000
 // à faire des requetes axios
 const corsOptions: cors.CorsOptions = {
-  origin: ['https://upside-7bqesp2ee-jacquespoulin.vercel.app/', 'https://upside.vercel.app/','https://upside-jacquespoulin.vercel.app/'],
+  origin: [
+    'https://upside-7bqesp2ee-jacquespoulin.vercel.app',
+    'https://upside.vercel.app',
+    'https://upside-jacquespoulin.vercel.app',
+  ],
   credentials: true,
 };
 
 // middleware cors
 app.use(cors(corsOptions));
-
+const allowedOrigins = ['https://upside.vercel.app'];
 //middleware perso pour ajouter les headers nécessaires à react-admin
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Credentials', "true");
+  const origin: string | any = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, PUT, POST, DELETE, OPTIONS'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
   next();
 });
-
-
 
 //middleware pour lire le body
 app.use(express.json());
