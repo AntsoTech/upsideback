70b5bee334f5ee5b063bef8ab80474ed2bcd7c22
Antso <95869782+AntsoTech@users.noreply.github.com>
2022-07-06 15:39:36 +0200

    Update db-config.ts

--- db-config.ts
+++ db-config.ts
@@ -1,15 +1,18 @@
 // importe le package dotenv pour accèder au .env
 import 'dotenv/config';
 // importe mysql pour se connecter à la base
-import mysql, { Connection } from 'mysql2';
+import mysql, { Pool } from 'mysql2';
 
 // créer l'objet connection
-const connection: Connection = mysql.createConnection({
+const connection: Pool = mysql.createPool({
   host: process.env.DB_HOST, // address of the server
   port: Number(process.env.DB_PORT), // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnection: true, 
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // exporte l'objet connection pour l'utiliser ailleurs
