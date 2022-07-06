fc04c5f7c8d41c4e633123e705f29993c3245758
AntsoTech <antso-rakotoson@hotmail.fr>
2022-07-06 23:16:58 +0200

    updated index to deploy cors

--- index.ts
+++ index.ts
@@ -24,16 +24,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS"));
-  next();
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  next()
 });
-
-app.all(('/',(req: Request, res: Response, next: NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS"));
-    next();
-  }));
 
 //middleware pour lire le body
 app.use(express.json());
